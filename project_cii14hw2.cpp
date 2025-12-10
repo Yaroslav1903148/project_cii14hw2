b3f038f832eb82c2591db9a4211c23f3f2a5f742
nazarenkoyaroslav <nazarenkoyarik19@gmail.com>
2025-12-10 15:17:13 +0200

    2.0 done

--- project_cii14hw2.cpp
+++ project_cii14hw2.cpp
@@ -4,14 +4,35 @@
 #include "My_pri_Queue.h"
 int main()
 {
-	Queue::My_pri_Queue arr("Cooker");
-	arr.push_priority(new Ingridient ("Cheese", 2));
-	arr.push_priority(new Ingridient ("Pasta", 20));
-	arr.push_priority(new Ingridient("Tomato", 5));
-	
+	//Queue::My_pri_Queue arr("Cooker");
+	//arr.push_priority(new Ingridient ("Cheese", 2));
+	//arr.push_priority(new Ingridient ("Pasta", 20));
+	//arr.push_priority(new Ingridient("Tomato", 5));
+	//
 
-	arr.showInfo();
-	
+	//arr.showInfo();
+	//
+
+
+    std::priority_queue<Ingridient> arr;
+
+    arr.push(Ingridient("Potato", 7));
+    arr.push(Ingridient("Carrot", 4));
+    arr.push(Ingridient("Meat", 10));
+    arr.push(Ingridient("Onion", 2));
+
+    std::cout << "===Start!!===\n";
+
+    while (!arr.empty()) {
+        Ingridient ing = arr.top();
+        arr.pop();
+
+        std::cout << "\nCooking: " << ing.getName() << " (" << ing.getTime() << " s)\n";
+
+        std::cout << "Done: " << ing.getName() << endl;
+    }
+
+    cout << "\nDone!!!";
 
 }
 
